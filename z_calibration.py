1f3139c0e8e2f1f20a1a94510eefcb664f8114b8
top-gun <detlev.rackow@googlemail.com>
2022-07-07 23:19:09 +0200

    If the offset is too large, dock the probe before raising an error condition (#63)
    run end gcode to dock the probe before raising an error

--- z_calibration.py
+++ z_calibration.py
@@ -286,6 +286,7 @@ class CalibrationState:
             z_positions = [p[2] for p in positions]
             if max(z_positions) - min(z_positions) > self.helper.tolerance:
                 if retries >= self.helper.retries:
+                    self.helper.end_gcode.run_gcode_from_command()
                     raise self.gcmd.error("Probe samples exceed tolerance")
                 self.gcmd.respond_info("Probe samples exceed tolerance."
                                        " Retrying...")
@@ -352,6 +353,7 @@ class CalibrationState:
                                   switch_zero, probe_zero, offset))
         # check max deviation
         if abs(offset) > self.helper.max_deviation:
+            self.helper.end_gcode.run_gcode_from_command()
             raise self.helper.printer.command_error("Offset is larger as"
                                                     " allowed: OFFSET=%.3f"
                                                     " MAX_DEVIATION=%.3f"
